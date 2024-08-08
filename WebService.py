118f6f06076cac1ca6bf1195ff75daf78a58d9d9
awj1052 <awj1052@naver.com>
2024-08-08 11:58:47 +0900

    feat: already stop

--- WebService.py
+++ WebService.py
@@ -1,11 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.alert import Alert
 import time
 
 import account
+
+# 클릭 후 alert 띄울 때까지 기다리는 시간
+CLICK_AFTER_INTERVAL = 0.8
+
+# alert 닫고 기다리는 시간
+WAIT_INTERVAL = 0.4
+
+# 대기열 등 오류 났을 때 기다리는 시간
+QUEUE_INTERVAL = 5
+
+# init 시 페이지 이동을 기다리는 시간
+PAGELOAD_INTERVAL = 5
+
+# alert에 단어가 포함되지 않으면 로그 메세지 print
+# EXCLUDED_WORD = '인원이 초과' # DEFAULT
+EXCLUDED_WORD = '인원이 초과' # DEFAULT
+
+# 수정 끝
 
 def zf(s):
     return str(s).zfill(2)
@@ -17,9 +34,9 @@ def get_current_time():
 def get_driver():
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--headless")
-    chrome_service = webdriver.ChromeService('/usr/lib/chromium-browser/chromedriver')
-    driver = webdriver.Chrome(options=chrome_options, service=chrome_service)
+    #chrome_options.add_argument("--headless")
+    #chrome_service = webdriver.ChromeService('/usr/lib/chromium-browser/chromedriver')
+    driver = webdriver.Chrome(options=chrome_options)#, service=chrome_service)
     return driver
 
 def init(driver):
@@ -28,34 +45,85 @@ def init(driver):
     driver.find_element(By.XPATH, '//*[@id="USER_PWD"]').send_keys(account.PW)
     driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/div/div[1]/div/button').click() # 로그인
     print(get_current_time(), 'login')
-    time.sleep(5)
+    time.sleep(PAGELOAD_INTERVAL)
     driver.find_element(By.LINK_TEXT, '수강신청 화면으로 이동').click() # 수강신청 화면 이동
     print(get_current_time(), 'move page')
-    time.sleep(5)
+    time.sleep(PAGELOAD_INTERVAL)
 
-def sugang(driver):
-
-    buttons = driver.find_elements(By.LINK_TEXT, '확정하기')
-    if len(buttons) == 1:
+def sugang_code(driver, codes):
+    if len(codes) == 0: 
         return True
+    
+    complete = []
+    for i in range(len(codes)):
+        a,b,c = codes[i].split("-")
+
+        A = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div/div/div/div/div/div[1]/div/div/div/div/div[5]/div/div/div[17]/div/div/div[8]/div/div/div[4]/div/div/div[13]/div/input')
+        A.clear(); A.send_keys(a)
+        
+        B = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div/div/div/div/div/div[1]/div/div/div/div/div[5]/div/div/div[17]/div/div/div[8]/div/div/div[4]/div/div/div[15]/div/input')
+        B.clear(); B.send_keys(b)
+        
+        C = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div/div/div/div/div/div[1]/div/div/div/div/div[5]/div/div/div[17]/div/div/div[8]/div/div/div[4]/div/div/div[17]/div/input')
+        C.clear(); C.send_keys(c)
+
+        driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div/div/div/div/div/div[1]/div/div/div/div/div[5]/div/div/div[17]/div/div/div[8]/div/div/div[4]/div/div/div[18]/a')\
+            .click()
+        
+        time.sleep(CLICK_AFTER_INTERVAL)
+
+        alert_text = check_alert(driver)
+
+        time.sleep(WAIT_INTERVAL)
+
+        if not EXCLUDED_WORD in alert_text: # message
+            print(get_current_time(), f'{alert_text} {a}-{b}-{c}')
+        if '이미 이수한 과목' in alert_text:
+            complete.append(i)
+    
+    for i in range(len(complete)-1, -1, -1):
+        codes.pop(complete[i])
+    return False
+
+def sugang(driver, complete):
+    buttons = driver.find_elements(By.LINK_TEXT, '확정하기')
+    if len(buttons) == 0:
+        return True
+    
+    if complete[-1] != len(buttons):
+        complete[-1] = len(buttons)
+        for i in range(len(complete)-1):
+            complete[i] = 0
+    else:
+        res = 0
+        for i in range(len(complete)-1):
+            if complete[i]: res += 1
+        if res == complete[-1]: return True
+
     for index, value in enumerate(buttons):
-        if index == 0: continue
+        if complete[index]: continue
         value.click()
-        time.sleep(0.8)
+        time.sleep(CLICK_AFTER_INTERVAL)
 
-        alert_text = '대기열'
-        try:
-            alert = Alert(driver)
-            alert_text = alert.text
-            alert.accept()
-        except:
-            print(get_current_time(), 'alert not found')
-            time.sleep(10)
+        alert_text = check_alert(driver)
 
-        time.sleep(0.4)        
+        time.sleep(WAIT_INTERVAL)        
 
-        if not '인원이 초과' in alert_text: # message
+        if not EXCLUDED_WORD in alert_text: # message
             xpath = f'/html/body/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div/div/div/div/div/div[1]/div/div/div/div/div[5]/div/div/div[20]/div/div/div/div/div[3]/div/div[2]/div/div[1]/div/div/div[{index+1}]/div[4]/div/div/div'
             element = driver.find_element(By.XPATH, xpath)
             print(get_current_time(), alert_text, element.text)
+        if '이미 이수한 과목' in alert_text:
+            complete[index] = 1
     return False
+
+def check_alert(driver):
+    alert_text = '대기열'
+    try:
+        alert = Alert(driver)
+        alert_text = alert.text
+        alert.accept()
+    except:
+        print(get_current_time(), 'alert not found')
+        time.sleep(QUEUE_INTERVAL)
+    return alert_text
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import WebService
 import time
 
+
+# 과목번호 아래와 같이 수정
+# target_code = [
+#     "1214-2003-01",
+#     "1214-2004-01",
+# ]
+
+target_code = [
+    "1214-2003-01"
+]
+
+# 예비 수강신청 아이템 개수 10보다 클 때만 수정
+BASKET = 10
+
+# 수정 끝
+
+# complete[-1]을 사용하기 위함
+BASKET += 1
+complete = [0]*BASKET
+
 def get_current_time():
     return WebService.get_current_time()
 
@@ -12,9 +32,11 @@ if __name__ == "__main__":
             print(get_current_time(), "init")
             WebService.init(driver)
 
-            flag = False
+            flag = True
             while 1:
-                flag = WebService.sugang(driver)
+                flag = WebService.sugang(driver, complete)
+                flag = WebService.sugang_code(driver, target_code) and flag
+                
                 if flag:
                     break
 
